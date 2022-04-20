e8ab071ee4f99ac4bf05c084f17cc25280dba22b
Xallver <jamesstanley11@gmail.com>
2022-04-20 18:32:57 -0400

    added all my get routes to filter out all possiblities.

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 const fs = require('fs');
-const reviews = require('./db/reviews.json');
-// Helper method for generating unique ids
-const uuid = require('./helpers/uuid');
+const allNotes = require('./db/db.json');
+// makes the server able to accept a "what port to listen on" parameter from the environment
+const PORT = process.env.PORT || 3001;
 
-const PORT = 3001;
 
-const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
+
+// Adding a static route for index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// res.json() allows us to return JSON instead of a buffer, string, or static file
+app.get('/api/notes', (req, res) => {
+  res.json(allNotes.slice(1));
+});
 
 
 
