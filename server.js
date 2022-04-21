5fe63aa1c88a2da3292957fcd493bded90622dc1
Xallver <jamesstanley11@gmail.com>
2022-04-20 20:08:50 -0400

    created new note function, commented out function, added post route to create the note

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const { v4: uuidv4 } = require('uuid');
 const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
 const allNotes = require('./db/db.json');
+
 // makes the server able to accept a "what port to listen on" parameter from the environment
 const PORT = process.env.PORT || 3001;
 
@@ -12,6 +14,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// res.json() allows us to return JSON instead of a buffer, string, or static file
+app.get('/api/notes', (req, res) => {  
+  res.json(allNotes.slice(1));
+});
 
 // Adding a static route for index.html
 app.get('/', (req, res) => {
@@ -26,15 +32,31 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-// res.json() allows us to return JSON instead of a buffer, string, or static file
-app.get('/api/notes', (req, res) => {
-  res.json(allNotes.slice(1));
+
+function generateNewNote(body, notesArray) {
+  const newNote = body;
+    //creates an empty array when returned false
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    
+    if (notesArray.length === 0)
+        notesArray.push(0);
+    //creates unique id
+    body.id = notesArray[0];
+    notesArray[0]++;
+    //pushes note
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+// creates the note
+app.post('/api/notes', (req, res) => {
+  const newNote = generateNewNote(req.body, allNotes);
+  res.json(newNote);
 });
-
-
-
-
-
 
 
 app.listen(PORT, () =>
